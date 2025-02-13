f342f3d9e97dfcf6a4b2dad0ad17aaa547be7147
Nahidul-Islam-Siam <siamnahidul093@gmail.com>
2025-02-13 22:58:21 +0600

    Implement authentication routes and hooks with Redux and React Router

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -1,1 +1,29 @@
- 
+import { useSelector, useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { setUser, logOut } from '../redux/features/auth/authSlice';
+import Cookies from 'js-cookie';
+
+export const useAuth = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const { user, token } = useSelector((state) => state.auth);
+
+  const login = (userData, token) => {
+    dispatch(setUser({ user: userData, token }));
+    Cookies.set('auth_token', token, { expires: 7, secure: true, sameSite: 'Strict' });
+  };
+
+  const logout = () => {
+    dispatch(logOut());
+    Cookies.remove('auth_token');
+    navigate('/login');
+  };
+
+  return {
+    user,
+    token,
+    isAuthenticated: !!token,
+    login,
+    logout
+  };
+}; 
--- src/routes/PublicRoute.jsx
+++ src/routes/PublicRoute.jsx
@@ -1,1 +1,10 @@
- 
+import { Navigate, Outlet } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+
+const PublicRoute = () => {
+  const { token } = useSelector((state) => state.auth);
+  
+  return !token ? <Outlet /> : <Navigate to="/dashboard" replace />;
+};
+
+export default PublicRoute; 
